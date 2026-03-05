8428057e4117d599e301e1bb33d8cb6432aa27b7
Vinayak Chiluka <chilukavinayak.p@gmail.com>
2026-03-05 12:31:00 +0530

    Add Spring Boot context load test

---
+++ src/test/java/com/tresvita/todo/TodoApplicationTest.java
@@ -0,0 +1,17 @@
+package com.tresvita.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Tresvita Todo Application Tests
+ * Managed by Wissen Team
+ */
+@SpringBootTest
+class TodoApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // Verifies that the Spring Boot application context loads successfully
+    }
+}
